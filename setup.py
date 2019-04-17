9c1a7c3dae31717982fcfccb1783096d977a2e34
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2019-04-17 06:34:45 +0200

    restoring python versions

--- setup.py
+++ setup.py
@@ -17,11 +17,12 @@ setup(
     long_description=read_description(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
